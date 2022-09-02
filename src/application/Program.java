a2112aadf35091c26e11eb6559e9ee1c92648cec
digarfi <digarfi@gmail.com>
2022-09-02 17:31:34 -0300

    Reusing instantiation changed

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,17 +8,12 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		/*Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "Maria", "maria@gmail.com", new Date(), 3000.00, obj);
-		System.out.println(seller);
-		*/
-		
+				
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST #1: Seller findById ===");
+		System.out.println();
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 
 	}
